2827f8201b39a2a5d00fb1cec90924458d8cc38c
iParsath <bilel.taktak@esprit.tn>
2020-04-16 20:15:24 +0100

    update

--- LaFerme/connexion.cpp
+++ LaFerme/connexion.cpp
@@ -8,8 +8,8 @@ bool Connexion::ouvrirConnexion()
 bool test=false;
    db=QSqlDatabase::addDatabase("QODBC");
 db.setDatabaseName("test-bd");
-db.setUserName("oracle");//inserer nom de l'utilisateur
-db.setPassword("oracle");//inserer mot de passe de cet utilisateur
+db.setUserName("parsath");//inserer nom de l'utilisateur
+db.setPassword("parsath");//inserer mot de passe de cet utilisateur
 
 
 if (db.open()) test = true;
---
+++ test-bd.sql
@@ -0,0 +1,19 @@
+CREATE TABLE mat_e
+(
+    id_mat number , nom varchar2(10),quantite number,date_ajout date
+);
+
+CREATE TABLE mat_a
+(
+    id_mat number , nom varchar2(10),etat number,date_ajout date
+);
+
+create table abris
+(
+    id_abris number, etat varchar2(100), type_abri varchar2(100)
+);
+
+create table animaux
+(
+id_ani number, type_ani varchar2(100), qualite varchar2(100), prix number
+);
