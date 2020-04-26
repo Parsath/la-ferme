590afeead187a40a26ccd671db737a48a7a64e0b
KenzaKhalfallah <58002990+KenzaKhalfallah@users.noreply.github.com>
2020-04-26 02:06:27 +0200

    Add files via upload

--- Kenza/mainwindow.cpp
+++ Kenza/mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "nourriture.h"
+#include "nourriture_v.h"
 #include <QString>
 #include <QMessageBox>
 #include <QObject>
@@ -9,7 +9,7 @@
 #include <QAbstractButton>
 #include <QIcon>
 #include <QtSql/QSqlDatabase>
-#include <QSqlQueryModel>
+#include <QtSql/QSqlQueryModel>
 #include <QComboBox>
 #include <QtWidgets>
 #include <QWidget>
@@ -20,7 +20,6 @@
 #include <QTextStream>
 #include <QTextDocument>
 
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -28,7 +27,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     ui->butSupp->setIcon(QIcon("../icone/icons8-poubelle-24.png"));
     ui->butSupp->icon();
-    ui->tabanimal->setModel(tmpnourriture.afficher());
+    ui->tabplante->setModel(tmpnourriture_v.afficher());
 }
 
 MainWindow::~MainWindow()
@@ -36,20 +35,19 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-//ajouter un aliment
 void MainWindow::on_butAjouter_clicked()
 {
 
     int id = ui->spinBoxID->text().toInt();
     QString aliment = ui->lineEdit_aliment->text();
-    QString animal = ui->lineEdit_2->text();
+    QString plante = ui->lineEdit_2->text();
     QString type = ui->lineEdit_3type->text();
     double qt = ui->doubleSpinBoxQT->text().toInt();
-    nourriture n(id,aliment,animal,type,qt);
+    nourriture_v n(id,aliment,plante,type,qt);
 
     // controle de saisie
 
-    if(nourriture::controle_id(id))
+    if(nourriture_v::controle_id(id))
         {
             QMessageBox::critical(nullptr, QObject::tr("Ajouter"),
                         QObject::tr("ID existant !.\n"
@@ -59,7 +57,7 @@ void MainWindow::on_butAjouter_clicked()
             ui->lineEdit_2->setText(0);
             ui->lineEdit_3type->setText(0);
             ui->doubleSpinBoxQT->setValue(0);
-            ui->tabanimal->setModel(tmpnourriture.afficher());
+            ui->tabplante->setModel(tmpnourriture_v.afficher());
             return;
         }
     else if(ui->spinBoxID->text().isEmpty() || ui->lineEdit_aliment->text().isEmpty() || ui->lineEdit_2->text().isEmpty() || ui->lineEdit_3type->text().isEmpty())
@@ -67,12 +65,12 @@ void MainWindow::on_butAjouter_clicked()
         QMessageBox::critical(nullptr, QObject::tr("Ajouter"),
                     QObject::tr("Veuillez remplir tous les champs.\n"
                                 "Click Cancel to exit."), QMessageBox::Cancel);
-        ui->tabanimal->setModel(tmpnourriture.afficher());
-    }
+        ui->tabplante->setModel(tmpnourriture_v.afficher());
+     }
     else
         {
             n.ajouter();
-            ui->tabanimal->setModel(tmpnourriture.afficher());
+            ui->tabplante->setModel(tmpnourriture_v.afficher());
             QMessageBox::information(nullptr, QObject::tr("Ajouter"),
                               QObject::tr("Aliment ajouté.\n"
                                           "Click Cancel to exit."), QMessageBox::Cancel);
@@ -84,16 +82,15 @@ void MainWindow::on_butAjouter_clicked()
         }
 }
 
-//supprimer data
 void MainWindow::on_butSupp_clicked()
 {
     QSqlQueryModel * model= new QSqlQueryModel();
-    model->setQuery("SELECT * FROM nourriture");
+    model->setQuery("SELECT * FROM nourriture_v");
     int id = ui->comboBox->currentText().toInt();
-    bool test=tmpnourriture.supprimer(id);
+    bool test=tmpnourriture_v.supprimer(id);
     if(test)
     {
-        ui->tabanimal->setModel(tmpnourriture.afficher());//refresh
+        ui->tabplante->setModel(tmpnourriture_v.afficher());//refresh
         QMessageBox::information(nullptr, QObject::tr("Supprimer un aliment"),
                           QObject::tr("Aliment supprimé.\n"
                                       "Click Cancel to exit."), QMessageBox::Cancel);
@@ -112,20 +109,19 @@ void MainWindow::on_butSupp_clicked()
     }
 }
 
-//modifier data
 void MainWindow::on_butModifier_clicked()
 {
 
     int id = ui->comboBox->currentText().toInt();
     QString aliment = ui->lineEdit_aliment->text();
-    QString animal = ui->lineEdit_2->text();
+    QString plante = ui->lineEdit_2->text();
     QString type = ui->lineEdit_3type->text();
     double qt = ui->doubleSpinBoxQT->text().toInt();
-    nourriture n(id,aliment,animal,type,qt);
+    nourriture_v n(id,aliment,plante,type,qt);
     bool test= n.modifier(id);
     if(test)
     {
-        ui->tabanimal->setModel(tmpnourriture.afficher());
+        ui->tabplante->setModel(tmpnourriture_v.afficher());
         QMessageBox::information(nullptr, QObject::tr("Modifier un aliment"),
                           QObject::tr("Aliment modifié.\n"
                                       "Click Cancel to exit."), QMessageBox::Cancel);
@@ -145,25 +141,22 @@ void MainWindow::on_butModifier_clicked()
     }
 }
 
-//afficher data
-void MainWindow::on_Nourriture_Animal_tabBarClicked(int index)
+void MainWindow::on_Nourriture_Vegetale_tabBarClicked(int index)
 {
-    ui->tabanimal->setModel(tmpnourriture.afficher());
+    ui->tabplante->setModel(tmpnourriture_v.afficher());
 }
 
-//liste des ID
 void MainWindow::on_butOK_clicked()
 {
-    ui->comboBox->setModel(tmpnourriture.afficher_id());
+    ui->comboBox->setModel(tmpnourriture_v.afficher_id());
 }
 
-//récupère data pour les modifier
 void MainWindow::on_comboBox_currentIndexChanged(const QString &arg1)
 {
     int id = ui->comboBox->currentText().toInt();
     QString res= QString::number(id);
     QSqlQuery qry;
-    qry.prepare("SELECT aliment, animal, type, qt FROM nourriture WHERE ID= :id");
+    qry.prepare("SELECT aliment, plante, type, qt FROM nourriture_v WHERE ID= :id");
     qry.bindValue(":id",res);
     if(qry.exec( ))
     {
@@ -177,75 +170,70 @@ void MainWindow::on_comboBox_currentIndexChanged(const QString &arg1)
     }
 }
 
-//recherche par ID
+
 void MainWindow::on_lineEditRecherche_textChanged(const QString &arg1)
 {
     QSqlQuery query;
     QSqlQueryModel *model= new QSqlQueryModel;
-    query.prepare("SELECT * FROM nourriture WHERE ID like :test ");
+    query.prepare("SELECT * FROM nourriture_v WHERE ID like :test ");
     query.bindValue(":test", arg1+"%");
     query.exec();
     model->setQuery(query);
-    ui->tabanimal->setModel(model);
+    ui->tabplante->setModel(model);
 }
 
-//trie par quantité
 void MainWindow::on_commandLinkButtonQt_clicked()
 {
-    ui->tabanimal->setModel(tmpnourriture.trieQt());
+    ui->tabplante->setModel(tmpnourriture_v.trieQt());
 }
 
-//trie par id
 void MainWindow::on_commandLinkButtonID_clicked()
 {
-   ui->tabanimal->setModel(tmpnourriture.trieId());
+   ui->tabplante->setModel(tmpnourriture_v.trieId());
 }
 
-//afficher l'id s'il existe
 void MainWindow::on_spinBoxID_valueChanged(const QString &arg1)
 {
     QSqlQuery query;
     QSqlQueryModel *model= new QSqlQueryModel;
-    query.prepare("SELECT * FROM nourriture WHERE ID like :test ");
+    query.prepare("SELECT * FROM nourriture_v WHERE ID like :test ");
     query.bindValue(":test", arg1+"%");
     query.exec();
     model->setQuery(query);
-    ui->tabanimal->setModel(model);
+    ui->tabplante->setModel(model);
 }
 
-
-//impression
 void MainWindow::on_radioButton_clicked()
 {
         QString strStream;
         QTextStream out(&strStream);
 
-        const int rowCount = ui->tabanimal->model()->rowCount();
-        const int columnCount = ui->tabanimal->model()->columnCount();
+        const int rowCount = ui->tabplante->model()->rowCount();
+        const int columnCount = ui->tabplante->model()->columnCount();
         QString TT = QDate::currentDate().toString("dd/MM/yyyy");
         out <<"<html>\n"
               "<head>\n"
                "<meta Content=\"Text/html; charset=Windows-1251\">\n"
-            << "<title>Liste de la nourriture animale <title>\n "
+            << "<title>Liste de la nourriture végétale <title>\n "
             << "</head>\n"
             "<body bgcolor=#ffffff link=#5000A0>\n"
             "<h3 style=\"font-style:italic; color:black;\">"+TT+"<br><br></h3>"
-            "<h1 style=\"text-align:center; font-style:italic; color:crimson;\"><strong> Liste de la nourriture animale <br><br> </strong></h1>"
+            "<h1 style=\"text-align:center; font-style:italic; color:crimson;\"><strong> Liste de la nourriture végétale <br><br> </strong></h1>"
             "<table style=\"text-align:center; font-size:20px; width:100%; border:2px dashed;\" border=1>\n "
               "</br> </br>";
         // headers
         out << "<thead><tr>";
         for (int column = 0; column < columnCount; column++)
-            if (!ui->tabanimal->isColumnHidden(column))
-                out << QString("<th>%1</th>").arg(ui->tabanimal->model()->headerData(column, Qt::Horizontal).toString());
+            if (!ui->tabplante->isColumnHidden(column))
+                out << QString("<th>%1</th>").arg(ui->tabplante->model()->headerData(column, Qt::Horizontal).toString());
         out << "</tr></thead>\n";
 
         // data table
         for (int row = 0; row < rowCount; row++) {
             out << "<tr>";
             for (int column = 0; column < columnCount; column++) {
-                if (!ui->tabanimal->isColumnHidden(column)) {
-                    QString data =ui->tabanimal->model()->data(ui->tabanimal->model()->index(row, column)).toString().simplified();
+                if (!ui->tabplante->isColumnHidden(column)) {
+                    QString data =ui->tabplante->model()->data(ui->tabplante->model()->index(row, column)).toString().simplified();
                     out << QString("<td bkcolor=0>%1</td>").arg((!data.isEmpty()) ? data : QString("&nbsp;"));
                 }
             }
@@ -266,3 +254,4 @@ void MainWindow::on_radioButton_clicked()
 
         delete document;
 }
+
